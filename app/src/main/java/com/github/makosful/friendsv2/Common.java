780f73f2eb23ac30267c848eaf16209ac17355bf
Axl <makosful@gmail.com>
2019-03-18 19:57:39 +0100

    Updated Common values

--- app/src/main/java/com/github/makosful/friendsv2/Common.java
+++ app/src/main/java/com/github/makosful/friendsv2/Common.java
@@ -3,9 +3,8 @@ package com.github.makosful.friendsv2;
 public class Common
 {
     public static final String intentFriendDetail = "friendDetail";
-
-    public static final int INTENT_REQUEST_ID_FRIEND_SMS = 1;
-    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 2;
+    public static final String intentFriendEdit= "friendEdit";
 
     public static final int CAMERA_REQUEST_CODE = 10;
+    public static final int FRIEND_EDIT_REQUEST_CODE = 20;
 }
