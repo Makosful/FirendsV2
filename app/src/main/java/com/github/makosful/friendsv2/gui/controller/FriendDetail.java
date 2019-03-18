41e1eb50560c99415ca2500efed0455420b8f95a
Axl <makosful@gmail.com>
2019-03-18 20:59:47 +0100

    Update FriendDetail.java

--- app/src/main/java/com/github/makosful/friendsv2/gui/controller/FriendDetail.java
+++ app/src/main/java/com/github/makosful/friendsv2/gui/controller/FriendDetail.java
@@ -1,10 +1,14 @@
 package com.github.makosful.friendsv2.gui.controller;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.makosful.friendsv2.Common;
 import com.github.makosful.friendsv2.R;
@@ -13,7 +17,12 @@ import com.github.makosful.friendsv2.be.Friend;
 public class FriendDetail extends AppCompatActivity
 {
     private static final String TAG = "FriendDetail";
+
     private Friend friend;
+
+    private TextView name;
+    private TextView phone;
+    private TextView email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,20 +30,45 @@ public class FriendDetail extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_detail);
 
-        final TextView name = findViewById(R.id.tv_name);
         friend = (Friend) getIntent().getSerializableExtra(Common.INTENT_FRIEND_DETAIL);
+        name = findViewById(R.id.tv_name);
         name.setText(friend.getName());
-        final TextView number = findViewById(R.id.txt_number);
+        phone = findViewById(R.id.txt_number);
         final String numberText = friend.getNumber() + "";
-        number.setText(numberText);
-        final TextView email = findViewById(R.id.txt_number);
+        phone.setText(numberText);
+        email = findViewById(R.id.txt_number);
         email.setText(friend.getEmail());
     }
 
     public void editFriend(View view)
     {
         Intent i = new Intent(this, FriendEdit.class);
-        i.putExtra(Common.intentFriendEdit, friend);
+        i.putExtra(Common.INTENT_FRIEND_EDIT, friend);
         startActivityForResult(i, Common.FRIEND_EDIT_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        Friend friend = (Friend)data.getExtras().get(Common.INTENT_FRIEND_EDIT_RESULT);
+        switch (resultCode)
+        {
+            case Activity.RESULT_OK: saveResult(friend); break;
+            default: Toast.makeText(this, "", Toast.LENGTH_SHORT).show(); break;
+        }
+    }
+
+    private void saveResult(Friend friend)
+    {
+        log("Logging changes to Friend");
+        this.friend = friend;
+        this.name.setText(friend.getName());
+        this.phone.setText(friend.getNumber());
+        this.email.setText(friend.getEmail());
+    }
+
+    private void log(String message)
+    {
+        Log.d(TAG, message);
     }
 }
