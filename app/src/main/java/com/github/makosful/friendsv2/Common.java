171e98c566984cb1ba63b17c04ef78a74c9c3658
Makos <32297912+Makosful@users.noreply.github.com>
2019-04-03 15:42:04 +0200

    Removed unused fields from Common

--- app/src/main/java/com/github/makosful/friendsv2/Common.java
+++ app/src/main/java/com/github/makosful/friendsv2/Common.java
@@ -8,10 +8,8 @@ public class Common
     public static final String DATA_FRIEND_DETAIL = "dataFriendDetail";
 
     public static final int FEATURE_REQUEST_CODE_CAMERA         = 101;
-    public static final int ACTIVITY_REQUEST_CODE_FRIEND_EDIT   = 201;
     public static final int ACTIVITY_REQUEST_CODE_FRIEND_EDIT   = 102;
     public static final int PERMISSION_REQUEST_CODE_CAMERA      = 301;
-    public static final int PERMISSION_REQUEST_CODE_LOCATION    = 302;
 
     public static final String DATABASE_NAME = "friend_app";
     public static final int DATABASE_VERSION = 1;
