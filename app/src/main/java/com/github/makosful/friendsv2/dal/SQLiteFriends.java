905424595faffe4ebee3770122a8c2d423e03651
Makos <32297912+Makosful@users.noreply.github.com>
2019-04-03 14:37:45 +0200

    Updated phone numbers on the SQLite Seed method, to be danish

--- app/src/main/java/com/github/makosful/friendsv2/dal/SQLiteFriends.java
+++ app/src/main/java/com/github/makosful/friendsv2/dal/SQLiteFriends.java
@@ -342,7 +342,7 @@ public class SQLiteFriends implements IStorage<Friend>
         friend.setAddress("14 Donald Trail");
         friend.setLatitude(35.23194);
         friend.setLongitude(40.32227);
-        friend.setPhone("571-343-7341");
+        friend.setPhone("74494110");
         friend.setEmail("tdanforth0@dion.ne.jp");
         friend.setWebsite("http://opera.com/pellentesque/ultrices.html");
         create(friend);
@@ -352,7 +352,7 @@ public class SQLiteFriends implements IStorage<Friend>
         friend.setAddress("838 4th Hill");
         friend.setLatitude(-0.5833333);
         friend.setLongitude(73.2333333);
-        friend.setPhone("914-611-0052");
+        friend.setPhone("14812627");
         friend.setEmail("kreijmers1@mayoclinic.com");
         friend.setWebsite("https://census.gov/vestibulum/aliquet/ultrices/erat.aspx");
         create(friend);
@@ -362,7 +362,7 @@ public class SQLiteFriends implements IStorage<Friend>
         friend.setAddress("44027 Chive Hill");
         friend.setLatitude(13.3670968);
         friend.setLongitude(103.8448134);
-        friend.setPhone("877-161-0186");
+        friend.setPhone("73742772");
         friend.setEmail("rhakes2@adobe.com");
         friend.setWebsite("https://yolasite.com/quisque.aspx");
         create(friend);
@@ -372,7 +372,7 @@ public class SQLiteFriends implements IStorage<Friend>
         friend.setAddress("76 Walton Pass");
         friend.setLatitude(38.8466225);
         friend.setLongitude(139.8744722);
-        friend.setPhone("723-549-8564");
+        friend.setPhone("45148295");
         friend.setEmail("msasser3@sphinn.com");
         friend.setWebsite("https://nature.com/nulla/neque.png");
         create(friend);
@@ -382,7 +382,7 @@ public class SQLiteFriends implements IStorage<Friend>
         friend.setAddress("0 Sunbrook Place");
         friend.setLatitude(40.0912332);
         friend.setLongitude(44.4037713);
-        friend.setPhone("595-182-0152");
+        friend.setPhone("70365509");
         friend.setEmail("cpeealess4@google.co.jp");
         friend.setWebsite("https://dailymail.co.uk/in/sagittis/dui/vel/nisl/duis/ac.png");
         create(friend);
@@ -392,7 +392,7 @@ public class SQLiteFriends implements IStorage<Friend>
         friend.setAddress("516 Debra Place");
         friend.setLatitude(36.962751);
         friend.setLongitude(100.901228);
-        friend.setPhone("602-780-8947");
+        friend.setPhone("69580095");
         friend.setEmail("cmcsherry5@paginegialle.it");
         friend.setWebsite("https://samsung.com/sapien/dignissim.json");
         create(friend);
@@ -402,7 +402,7 @@ public class SQLiteFriends implements IStorage<Friend>
         friend.setAddress("5 Mockingbird Point");
         friend.setLatitude(9.939624);
         friend.setLongitude(126.065213);
-        friend.setPhone("858-901-5233");
+        friend.setPhone("57057137");
         friend.setEmail("lmckeating6@amazon.de");
         friend.setWebsite("https://woothemes.com/erat/vestibulum/sed.jpg");
         create(friend);
@@ -412,7 +412,7 @@ public class SQLiteFriends implements IStorage<Friend>
         friend.setAddress("9 Fallview Plaza");
         friend.setLatitude(59.330173);
         friend.setLongitude(18.0551861);
-        friend.setPhone("768-161-9882");
+        friend.setPhone("47435961");
         friend.setEmail("bberrisford7@cdc.gov");
         friend.setWebsite("http://europa.eu/eleifend/donec/ut/dolor/morbi/vel.xml");
         create(friend);
@@ -422,7 +422,7 @@ public class SQLiteFriends implements IStorage<Friend>
         friend.setAddress("9279 Main Trail");
         friend.setLatitude(9.338241);
         friend.setLongitude(-66.2575425);
-        friend.setPhone("617-152-0825");
+        friend.setPhone("50258293");
         friend.setEmail("aartrick8@smh.com.au");
         friend.setWebsite("https://macromedia.com/ut/volutpat.jsp");
         create(friend);
@@ -432,7 +432,7 @@ public class SQLiteFriends implements IStorage<Friend>
         friend.setAddress("19 Cody Lane");
         friend.setLatitude(-38.9898711);
         friend.setLongitude(175.8087485);
-        friend.setPhone("973-668-5925");
+        friend.setPhone("77620275");
         friend.setEmail("ebraidwood9@washington.edu");
         friend.setWebsite("http://accuweather.com/cursus/vestibulum/proin.jsp");
         create(friend);
