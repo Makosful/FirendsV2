1b4be8a758136d18f1c288c9a5424c706992bf63
Dankphetamine <asge0907@easv365.dk>
2019-04-05 01:30:28 +0200

    Revert "Test, followed google guide for map. Will revert"
    This reverts commit 1014a57ec9143b7516c7cdc17c7ff93ee61e563a.

--- app/src/main/java/com/github/makosful/friendsv2/gui/controller/MapsActivity.java
+++ app/src/main/java/com/github/makosful/friendsv2/gui/controller/MapsActivity.java
@@ -1,19 +1,16 @@
 package com.github.makosful.friendsv2.gui.controller;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.ContextCompat;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import com.github.makosful.friendsv2.Common;
 import com.github.makosful.friendsv2.IMapCallBack;
 import com.github.makosful.friendsv2.R;
@@ -21,66 +18,31 @@ import com.github.makosful.friendsv2.be.Friend;
 import com.github.makosful.friendsv2.gui.model.MyLocationListener;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.GeoDataClient;
-import com.google.android.gms.location.places.PlaceDetectionClient;
-import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, IMapCallBack {
 
     public static String TAG = "mapActivity";
 
     private int PERMISSION_CODE = 1337;
+    private GoogleMap mMap;
     private Friend friend;
     MarkerOptions friendMarker;
     Location deviceLocation;
     LocationListener locationListener;
     LocationManager locationManager;
-
-    private GoogleMap mMap;
-    private CameraPosition mCameraPosition;
-
-    // The entry points to the Places API.
-    private GeoDataClient mGeoDataClient;
-    private PlaceDetectionClient mPlaceDetectionClient;
-
-    // The entry point to the Fused Location Provider.
-    private FusedLocationProviderClient mFusedLocationProviderClient;
-
-    // A default location (Sydney, Australia) and default zoom to use when location permission is
-    // not granted.
-    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
-    private static final int DEFAULT_ZOOM = 15;
-    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    private boolean mLocationPermissionGranted;
-
-    private static final String KEY_LOCATION = "location";
-
-    // The geographical location where the device is currently located. That is, the last-known
-    // location retrieved by the Fused Location Provider.
-    private Location mLastKnownLocation;
+    private FusedLocationProviderClient mfLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-        // Construct a GeoDataClient.
-        mGeoDataClient = Places.getGeoDataClient(this, null);
-
-        // Construct a PlaceDetectionClient.
-        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
-
-        // Construct a FusedLocationProviderClient.
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         Button btnShowDirections = findViewById(R.id.btnShowDirections);
         btnShowDirections.setOnClickListener(new View.OnClickListener() {
@@ -91,7 +53,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 showDirectionsToFriend(v);
             }
         });
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -99,19 +60,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         log("Retrieving Friend from extras");
         friend = (Friend) getIntent().getExtras().get(Common.INTENT_MAP_ACTIVITY);
+        mfLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        // Retrieve location from saved instance state.
-        if (savedInstanceState != null) {
-            mLastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        if (mMap != null) {
-            outState.putParcelable(KEY_LOCATION, mLastKnownLocation);
-            super.onSaveInstanceState(outState);
-        }
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = null;
+        requestGPSPermissions();
     }
 
     /**
@@ -127,30 +80,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Do other setup activities here too, as described elsewhere in this tutorial.
-
-        // Turn on the My Location layer and the related control on the map.
-        updateLocationUI();
-
-        // Get the current location of the device and set the position of the map.
-        getDeviceLocation();
+        // Add latlng + marker
+        friendMarker = new MarkerOptions().position(new LatLng(this.friend.getLatitude(), this.friend.getLongitude()));
+        //TODO finish by re-adding custom user image icon instead of standard red pointer
+        mMap.addMarker(friendMarker.title(this.friend.getName())/*.icon(BitmapDescriptorFactory.fromBitmap(this.friend.getPicture()))*/);
+        // center map
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(this.friend.getLatitude(), this.friend.getLongitude())));
     }
 
     private void showDirectionsToFriend(View view) {
-        //deviceLocation = mLastKnownLocation;
-        //log("CURRENT LIVE LOCATION: " + deviceLocation);
+        deviceLocation = getLastKnownLocation();
+        log("CURRENT LIVE LOCATION: " + deviceLocation);
     }
 
     private Location getLastKnownLocation() {
 
-        Boolean GPSPermissionGiven;
+        Boolean GPSPermissionGiven = false;
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
             GPSPermissionGiven = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-        } else {
+        }
+        else {
             GPSPermissionGiven = true;
         }
-        return GPSPermissionGiven ? locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER) : null;
+        return GPSPermissionGiven ? locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER): null;
     }
 
     protected void startListening() {
@@ -163,7 +116,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
             GPSPermissionGiven = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) ==
                     PackageManager.PERMISSION_GRANTED;
-        } else {
+        }
+        else {
             GPSPermissionGiven = true;
         }
         if (GPSPermissionGiven)
@@ -175,82 +129,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             log("Could not register location listener..");
     }
 
-    private void getLocationPermission() {
-        /*
-         * Request location permission, so that we can get the location of the
-         * device. The result of the permission request is handled by a callback,
-         * onRequestPermissionsResult.
-         */
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mLocationPermissionGranted = true;
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        }
-    }
+    private void requestGPSPermissions() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-        mLocationPermissionGranted = false;
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mLocationPermissionGranted = true;
-                }
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_DENIED) {
+
+                log("Location permission denied");
+                String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+
+                requestPermissions(permissions, PERMISSION_CODE);
+                return;
+            } else
+            {
+                log("Location permission granted!");
             }
         }
-        updateLocationUI();
+        else
+            log("Location permission only granted by manifest");
     }
 
-    private void updateLocationUI() {
-        if (mMap == null) {
-            return;
-        }
-        try {
-            if (mLocationPermissionGranted) {
-                mMap.setMyLocationEnabled(true);
-                mMap.getUiSettings().setMyLocationButtonEnabled(true);
-            } else {
-                mMap.setMyLocationEnabled(false);
-                mMap.getUiSettings().setMyLocationButtonEnabled(false);
-                mLastKnownLocation = null;
-                getLocationPermission();
-            }
-        } catch (SecurityException e)  {
-            Log.e("Exception: %s", e.getMessage());
-        }
-    }
-
-    private void getDeviceLocation() {
-        /*
-         * Get the best and most recent location of the device, which may be null in rare cases when a location is not available.
-         */
-        try {
-            if (mLocationPermissionGranted) {
-                Task locationResult = mFusedLocationProviderClient.getLastLocation();
-                locationResult.addOnCompleteListener(this, new OnCompleteListener() {
-                    @Override
-                    public void onComplete(@NonNull Task task) {
-                        if (task.isSuccessful()) {
-                            // Set the map's camera position to the current location of the device.
-                            mLastKnownLocation = (Location) task.getResult();
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
-                        } else {
-                            Log.d(TAG, "Current location is null. Using defaults.");
-                            Log.e(TAG, "Exception: %s", task.getException());
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
-                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
-                        }
-                    }
-                });
-            }
-        } catch(SecurityException e)  {
-            Log.e("Exception: %s", e.getMessage());
-        }
-    }
-
-    private void log(String message) {
+    private void log(String message)
+    {
         Log.d(TAG, message);
     }
 
